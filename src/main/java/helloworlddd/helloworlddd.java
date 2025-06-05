0ee911b3bafba39471d570d6e826106acd5ca146
lalit <lalit@Lalithya>
2025-06-05 12:46:41 +0530

    first commit

---
+++ src/main/java/helloworlddd/helloworlddd.java
@@ -0,0 +1,11 @@
+package helloworlddd;
+
+public class helloworlddd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+
+	}
+
+}
